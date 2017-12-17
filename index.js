4ce1458012bc75406da4aee8304057060150c2a5
Monika Williams <monika.m.williams@gmail.com>
2017-12-17 07:43:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  handlebarsSetup();
+  initForm();
 }
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
+  init();
 })
